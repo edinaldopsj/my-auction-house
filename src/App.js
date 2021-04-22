4aec83bf8c2eb97bc8b1741fae761e8668da7358
edinaldopsj <edinaldopsj92@gmail.com>
2021-04-22 17:58:04 -0300

    Layout - starting login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { Button, TextField } from '@material-ui/core';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <main className="App-main">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to React&apos;s My Auction House</p>
-      </header>
+        <TextField autoFocus type="text" label="Login" variant="outlined" />
+        <TextField type="password" label="Password" variant="outlined" />
+        <Button variant="contained" color="primary">
+          Enter
+        </Button>
+      </main>
     </div>
   );
 }
